548b4529f9c15f3a615b7fa60a3412d964c2355a
Bolshakov Dmitry <bolshakoff.dima@yandex.ru>
2024-10-23 17:02:18 +0300

    финальная версия

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -4,8 +4,11 @@ import UIKit
 final class MovieQuizViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        showPicture()
+        showPicture() // показ первой картинки для старта квиза
     }
+    
+    @IBOutlet private weak var picture: UIImageView!
+    @IBOutlet private weak var counterLabel: UILabel!
     
     private let questions: [QuizQuestion] = [
         QuizQuestion(
@@ -54,42 +57,45 @@ final class MovieQuizViewController: UIViewController {
     private var correctAnswers = 0
     private var gamesPlayed = 0
     private var record = 0
-    private var bestResultModelArray: [bestResultModel] = []
+    private var resultBase = bestResultModel(record: 0, timeWhenFinished: Date())
     
-    struct QuizQuestion {
+    private struct QuizQuestion {
         let image: String
         let rating: Double
         let correctAnswer: Bool
     }
     
-    struct QuizResultsViewModel {
+    private struct QuizResultsViewModel {
         let title: String
         let text: String
         let buttonText: String
     }
     
-    
     struct bestResultModel{
-        let record: Int
-        let timeWhenFinished: Date
+        var record: Int
+        var timeWhenFinished: Date
     }
     
-    @IBOutlet private weak var picture: UIImageView!
-    @IBOutlet private weak var counterLabel: UILabel!
+
     
-    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+    @IBOutlet private weak var yesButton: UIButton!
+    @IBOutlet private weak var noButton: UIButton!
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) { //сравниваем результат ответа с правильным из массива и вызываем метод для отображения результат ответа(ввиде цветной марки вокруг картинки)
+        yesButton.isEnabled = false // блок клавиши на время показа рамки рехультата (1 сек)
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
     @IBAction private func noButtonClicked(_ sender: UIButton) {
+        noButton.isEnabled = false
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
-    private func showAnswerResult(isCorrect: Bool) {
+    private func showAnswerResult(isCorrect: Bool) { // окраска картинки в зеленый/красный цвет в завимиости от правильности ответа
         picture.layer.masksToBounds = true
         picture.layer.borderWidth = 8
         picture.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
@@ -97,37 +103,43 @@ final class MovieQuizViewController: UIViewController {
             correctAnswers += 1 // если ответ корректный инкрементируем correctAnswers
         }
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-               self.showNextQuestionOrResults()
-            
+            self.showNextQuestionOrResults()
+            self.yesButton.isEnabled = true
+            self.noButton.isEnabled = true
            }
         
     }
     
     private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questions.count - 1 { // конец квиза
+        if currentQuestionIndex == questions.count - 1 { // конец квиза и логика для отобрадения алерты с результатми
             gamesPlayed += 1
-            if correctAnswers > record {
+            let date = Date()
+            if gamesPlayed == 1 {
+                resultBase.record = correctAnswers
+                resultBase.timeWhenFinished = date
                 record = correctAnswers
             }
-            let date = Date()
-            let resultModel = bestResultModel(record: record,
-                                              timeWhenFinished: date)
+            if correctAnswers > record {
+                record = correctAnswers
+                resultBase.record = correctAnswers
+                resultBase.timeWhenFinished = date
+            }
             let winPercentage = Double(correctAnswers) / Double(questions.count) * 100
-            let text = "Ваш результат: \(correctAnswers)/10 \n Количество сыгранных квизов: \(gamesPlayed) \n Рекорд: \(calculateBestResult(resultModel: resultModel)) \n Средняя точность: \(winPercentage.formatWithTwoDecimalPlaces())%"
+            let text = "Ваш результат: \(correctAnswers)/10 \n Количество сыгранных квизов: \(gamesPlayed) \n Рекорд: \(resultBase.record) \(resultBase.timeWhenFinished.dateTimeString) \n Средняя точность: \(winPercentage.formatWithTwoDecimalPlaces())%"
             let viewModel = QuizResultsViewModel( // 2
                 title: "Этот раунд окончен!",
                 text: text,
                 buttonText: "Сыграть ещё раз")
             showResults(quiz: viewModel)
-        } else { // 2
+        } else { // показывем след картинку
             currentQuestionIndex += 1
             counterLabel.text = "\(currentQuestionIndex + 1)/10"
-            showPicture() // показываем след картинку
+            showPicture()
         }
         
     }
     
-    private func showResults(quiz result: QuizResultsViewModel) {
+    private func showResults(quiz result: QuizResultsViewModel) { // показывает алерту
         
         let alert = UIAlertController(
             title: result.title,
@@ -135,7 +147,7 @@ final class MovieQuizViewController: UIViewController {
             preferredStyle: .alert)
         
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
-            self.currentQuestionIndex = 0
+            self.currentQuestionIndex = 0 // обнуляем поля для нового квиза
             self.correctAnswers = 0
             self.showPicture()
             self.counterLabel.text = "1/10"
@@ -147,21 +159,9 @@ final class MovieQuizViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-    private func showPicture() {
+    private func showPicture() { // обновляет картинку и убирает цвет рамки
         picture.image = UIImage(named: questions[currentQuestionIndex].image) ?? UIImage()
         picture.layer.borderWidth = 0
         
     }
-    
-    private func calculateBestResult(resultModel: bestResultModel) -> String {
-        if bestResultModelArray.isEmpty == true {
-            bestResultModelArray.append(resultModel)
-            return "\(resultModel.record): \(resultModel.timeWhenFinished.dateTimeString)"
-        }
-        if resultModel.record > bestResultModelArray[0].record {
-            return "\(resultModel.record): \(resultModel.timeWhenFinished.dateTimeString)"
-        } else { return "\(bestResultModelArray[0].record): \(bestResultModelArray[0].timeWhenFinished.dateTimeString)"
-        }
-    }
-    
 }
